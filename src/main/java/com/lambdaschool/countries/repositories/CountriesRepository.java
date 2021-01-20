82e104115e1531c6695621127546da7fada8de6a
Jordan Hanson <JORDANMERCEDES22@GMAIL.COM>
2021-01-19 22:54:58 -0700

    controllers models repositories made and class Country created

---
+++ src/main/java/com/lambdaschool/countries/repositories/CountriesRepository.java
@@ -0,0 +1,4 @@
+package com.lambdaschool.countries.repositories;
+
+public interface CountriesRepository {
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+
+DELETE
+FROM countries;
+
+INSERT INTO countries(countryid, name, population, landmasskm2, medianage)
+VALUES (39, 'Afghanistan', 37209007, 652860, 19 );
+
+alter sequence hibernate_sequence restart with 5;
